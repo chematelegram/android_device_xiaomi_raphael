a2922c6b061615715de8c45b57667b2b13f0285a
chematelegram <chfondia@gmail.com>
2023-09-06 10:00:04 +0000

    raphael: Remove some GApps

--- device.mk
+++ device.mk
@@ -432,6 +432,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_raphael
 
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
