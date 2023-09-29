3176f4a5ca79904db21b0cb2eaaeab904b7e3774
chematelegram <chfondia@gmail.com>
2023-09-29 22:58:41 +0200

    raphael: Remove some GApps

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,8 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps GoogleFeedback TipsPrebuilt Drive YouTube Videos
-LOCAL_OVERRIDES_PACKAGES := Photos RecorderPrebuilt Chrome FilesPrebuilt GoogleTTS
+LOCAL_OVERRIDES_PACKAGES := Maps GoogleFeedback TipsPrebuilt Drive YouTube Videos SoundAmplifierPrebuilt CalendarGooglePrebuilt
+LOCAL_OVERRIDES_PACKAGES += Photos RecorderPrebuilt Chrome Chrome-Stub FilesPrebuilt GoogleTTS CalculatorGooglePrebuilt
+LOCAL_OVERRIDES_PACKAGES += Recorder SafetyHubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
